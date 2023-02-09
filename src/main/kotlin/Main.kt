12596ed64e61e42eea555c3d7d2c763b0c78fde0
Vallary Buraje <vallaryburaje@gmail.com>
2023-02-09 20:50:50 +0300

    first assignment commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,39 @@
+fun main(){
+    printName("Vallary")
+    println(65/8)
+    var sum = add(7,3,4,8)
+    println(sum)
+    fact()
+}
+fun printName(name: String){
+    var name = "Vallary"
+    println("hello"+ " "+name)
+}
+fun divide ( num1: Int, num2:  Int ):Int {
+    var divide = num1/num2
+    return (divide)
+    println(divide)
+
+
+
+}
+
+fun add ( num1: Int, num2: Int, num3: Int, num4: Int):Int{
+    var sum = num1 +num2 +num3 +num4
+    return (sum)
+
+}
+fun fact () {
+    var fact = "I can make it"
+    println(fact)
+}
+
+
+
+
+
+
+
+
+
+
